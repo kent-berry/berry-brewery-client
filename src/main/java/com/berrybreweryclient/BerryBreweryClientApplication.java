4fb57c172f643af1cc6aa685e431a0a69e4dcfbf
Kent Berry <kentwberry@protonmail.com>
2023-03-03 14:55:11 -0500

    initial project add

---
+++ src/main/java/com/berrybreweryclient/BerryBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package com.berrybreweryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerryBreweryClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BerryBreweryClientApplication.class, args);
+    }
+
+}
