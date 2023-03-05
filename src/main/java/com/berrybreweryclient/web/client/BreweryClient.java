aaa8baec7aa3b3285f1f3e19a97940605c830301
Kent Berry <kentwberry@protonmail.com>
2023-03-05 15:26:57 -0500

    added models, client using resttemplate, resttemplate custom connection managers, and circleci

---
+++ src/main/java/com/berrybreweryclient/web/client/BreweryClient.java
@@ -0,0 +1,62 @@
+package com.berrybreweryclient.web.client;
+
+import com.berrybreweryclient.web.model.BeerDto;
+import com.berrybreweryclient.web.model.CustomerDto;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.UUID;
+
+@Component
+@ConfigurationProperties(prefix = "berry.brewery", ignoreUnknownFields = false)
+public class BreweryClient {
+
+    public static final String CUSTOMER_PATH_V1 = "/api/v1/customer";
+    public final String BEER_PATH_V1 = "/api/v1/beer";
+    private String apihost;
+
+    private final RestTemplate restTemplate;
+
+    public BreweryClient(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public BeerDto getBeerById(UUID uuid){
+        return restTemplate.getForObject(apihost + BEER_PATH_V1 + "/" + uuid.toString(), BeerDto.class);
+    }
+
+    public URI saveNewBeer(BeerDto beerDto){
+        return restTemplate.postForLocation(apihost + BEER_PATH_V1, beerDto);
+    }
+
+    public void updateBeer(UUID uuid, BeerDto beerDto){
+        restTemplate.put(apihost + BEER_PATH_V1 + "/" + uuid.toString(), beerDto);
+    }
+
+    public void deleteBeer(UUID uuid){
+        restTemplate.delete(apihost + BEER_PATH_V1 + "/" + uuid.toString());
+    }
+
+    public void setApihost(String apihost) {
+        this.apihost = apihost;
+    }
+
+    public CustomerDto getCustomerById(UUID customerId) {
+        return restTemplate.getForObject(apihost+ CUSTOMER_PATH_V1 + "/" + customerId.toString(), CustomerDto.class);
+    }
+
+    public URI saveNewCustomer(CustomerDto customerDto) {
+        return  restTemplate.postForLocation(apihost + CUSTOMER_PATH_V1, customerDto);
+    }
+
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        restTemplate.put(apihost + CUSTOMER_PATH_V1 + "/" + customerId, customerDto);
+    }
+
+    public void deleteCustomer(UUID customerId) {
+        restTemplate.delete(apihost + CUSTOMER_PATH_V1 + "/" + customerId);
+    }
+}
---
+++ src/main/java/com/berrybreweryclient/web/config/BlockingRestTemplateCustomizer.java
@@ -0,0 +1,58 @@
+package com.berrybreweryclient.web.config;
+
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
+
+    private final Integer maxTotalConnections;
+    private final Integer defaultMaxConnections;
+    private final Integer connectionRequestTimeout;
+    private final Integer socketTimeout;
+
+    public BlockingRestTemplateCustomizer(@Value("${berry.connection.maxtotalconnections}") Integer maxTotalConnections,
+                                          @Value("${berry.connection.defaultmaxtotalconnections}") Integer defaultMaxConnections,
+                                          @Value("${berry.connection.connectionrequesttimeout}") Integer connectionRequestTimeout,
+                                          @Value("${berry.connection.sockettimeout}") Integer socketTimeout) {
+        this.maxTotalConnections = maxTotalConnections;
+        this.defaultMaxConnections = defaultMaxConnections;
+        this.connectionRequestTimeout = connectionRequestTimeout;
+        this.socketTimeout = socketTimeout;
+    }
+
+    public ClientHttpRequestFactory clientHttpRequestFactory(){
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(maxTotalConnections);
+        connectionManager.setDefaultMaxPerRoute(defaultMaxConnections);
+
+        RequestConfig requestConfig = RequestConfig
+                .custom()
+                .setConnectionRequestTimeout(connectionRequestTimeout)
+                .setSocketTimeout(socketTimeout)
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients
+                .custom()
+                .setConnectionManager(connectionManager)
+                .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+
+        return new HttpComponentsClientHttpRequestFactory(httpClient);
+    }
+
+    @Override
+    public void customize(RestTemplate restTemplate) {
+        restTemplate.setRequestFactory(this.clientHttpRequestFactory());
+    }
+}
